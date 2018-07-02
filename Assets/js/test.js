858d7f5bcd6981164bbf6566b518994437a7b229
AKHIL RAVIPATI <S530485@nwmissouri.edu>
2018-07-02 11:38:36 -0500

    Add javascript

---
+++ Assets/js/test.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var x = document.getElementById("mySelect").value;
+    document.getElementById("selectEmployee").innerHTML = x;
+    document.getElementById("myModal1")
+
+}
