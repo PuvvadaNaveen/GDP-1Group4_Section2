cafa8f94cbca51b5f796b0ed15d704b887ac37a1
Shiva Kumar Mutukula <mutukulashiva099.com>
2018-08-31 09:52:01 -0500

    Add mail sending feature

--- main.js
+++ main.js
@@ -14,6 +14,27 @@ app.use(express.static(__dirname + '/Views'));
 
 const routes = require('./routes/index.js')
 app.use('/', routes)
+app.post("/mail",function(request, response){
+    var api_key = 'key-40415a0687ee485304994a7878f9a4a0';
+    var domain = 'sandboxaeb7264ac0c34508b90f37828951db15.mailgun.org';
+    var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+     
+    var data = {
+      from: 'Stephi Designer <me@samples.mailgun.org>',
+      to: 's530479@nwmissouri.edu',
+      subject: "Collecting performers details",
+      text: "Please fill the basic information form"
+    };
+     
+    mailgun.messages().send(data, function (error, body) {
+      console.log(body);
+      if(!error)
+        response.send("Mail Sent Successfully");
+      else
+        response.send("Mail not sent please check again");
+    });
+  })
+
 
 // set up an http request logger to log every request automagically
 app.use(logger("dev"));     // app.use() establishes middleware functions
@@ -23,7 +44,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(function (request, response) {
     response.status(404).render('404.ejs');
 });
-
 // Listen for an application request on port 8089
 http.listen(8089, function () {
   console.log('project is listening on http://127.0.0.1:8089/');
