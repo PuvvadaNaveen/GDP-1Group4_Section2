f45fac8132507e1c6a88a81de4bf65d2f354e618
Puvvada_Naveen <naveen6kumar.71@gmail.com>
2018-09-11 16:01:58 -0500

    Save form data to database

--- models/form.js
+++ models/form.js
@@ -12,6 +12,75 @@ var FormSchema = mongoose.Schema({
 	},
 	phonenumber: {
 		type: Number
+	},
+	textchoice: {
+		type: String
+	},
+	email: {
+		type: String
+	},
+	agreement: {
+		type: String
+	},
+	dominanthand: {
+		type: String
+	},
+	height: {
+		type: String
+	},
+	estimatedweight: {
+		type: Number
+	},
+	eyewearchoice: {
+		type: String
+	},
+	hair: {
+		type: String
+	},
+	facialhai: {
+		type: String
+	},
+	earschoice: {
+		type: String
+	},
+	otherpiercings: {
+		type: String
+	},
+	tattoos: {
+		type: String
+	},
+	tattooagreement: {
+		type: String
+	},
+	comments: {
+		type: String
+	},
+	shoes: {
+		type: String
+	},
+	danceshoes: {
+		type: String
+	},
+	shirtsize: {
+		type: String
+	},
+	pants: {
+		type: String
+	},
+	brasize: {
+		type: String
+	},
+	ring: {
+		type: String
+	},
+	allergy: {
+		typr: String
+	},
+	other: {
+		type: String
+	},
+	allergies: {
+		type: String
 	}
 });
 
