4be2846969a93a078453a86a34ff5ede7f77430f
meghana <meghanamayaluri@gmail.com>
2018-09-18 23:24:43 -0500

    Add field for selecting clothing item and color

--- Assets/js/test.js
+++ Assets/js/test.js
@@ -1,13 +1,22 @@
 function myFunction() {
     var x = document.getElementById("mySelect").value;
     document.getElementById("selectEmployee").innerHTML = x;
+}
     // document.getElementById("myModal1")
+    function myFunction1(){
+    var y = document.getElementById("mySelect1").value;
+    document.getElementById("selectItem").innerHTML = y;
+    }
+    function myFunction2(){
+        
+    var z = document.getElementById("mySelect2").value;
+    document.getElementById("selectColor").innerHTML = z;
 }
 function emailSent(){
     window.alert("Email sent");
 }
 
 function validateMayWeTextYou(){
-    var checkedYes = document.getElementById();
+    var checkedYes = document.getElementById()
 
 }
