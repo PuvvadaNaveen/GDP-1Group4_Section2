d2635984542ce353dd2bb162a1974f009ec80655
AKHIL RAVIPATI <S530485@nwmissouri.edu>
2018-10-02 21:38:57 -0500

    Fix email routing for forget password

--- routes/users.js
+++ routes/users.js
@@ -171,7 +171,7 @@ router.post('/resetpassword', function(req, res, next) {
 		  subject: 'Node.js Password Reset',
 		  text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
 			'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-			'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+			'http://' + req.headers.host + '/users/reset/' + token + '\n\n' +
 			'The link expires in 1 hour. \n' +
 			'If you did not request this, please ignore this email and your password will remain unchanged.\n'
 		};
